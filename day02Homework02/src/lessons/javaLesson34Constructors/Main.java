d6ee308c6e6fe9921e7204c8d80f2546dd3db563
YasinCelikSoftware <107568842+YasinCelikSoftware@users.noreply.github.com>
2022-09-27 19:35:09 +0300

    Update Main.java

--- day02Homework02/src/lessons/javaLesson34Constructors/Main.java
+++ day02Homework02/src/lessons/javaLesson34Constructors/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Product product = new Product(1,"Laptop","Asus Laptop",5000,3,"Siyah");
+		Product product = new Product(1,"Laptop","Asus Laptop",5000,3,"Black");
 		
 		/*
 		 * product.setName("Laptop"); product.setId(1);
